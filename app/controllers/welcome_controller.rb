b16d464e679112824a78bc97fbf8c9448fc9dade
Rodrigo Aliste P <rodrigo@joldit.com>
2012-10-27 18:39:01 -0300

    Adds voting prototype, use real db objects on index.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @candidatos = {}
+    _candidatos = Candidato.all
+    _candidatos.each do |candidato|
+      @candidatos[candidato.name] = candidato
+    end
+  end
+
+  def vote
+    # TODO validate and count vote
+    redirect_to url_for(:action => 'index')
   end
 end
