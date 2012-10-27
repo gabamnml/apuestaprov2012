8dc86285c7fb71f51c57bff6df5b6dd257e1fb14
Gabriel Aramburu <aramburu.gabriel@gmail.com>
2012-10-27 19:11:10 -0300

    Load candidatos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+josefa = Candidato.create! :name => 'Josefa', :votes => 0
+puts "Your candidate is #{josefa.name} and votes #{josefa.votes}"
+
+labbe = Candidato.create! :name => 'Labbe', :votes => 0
+puts "Your candidate is #{labbe.name} and votes #{labbe.votes}"
