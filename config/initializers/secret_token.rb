bcaaeca6430b7a6be649232088057157bfe731e0
Gabriel Aramburu <aramburu.gabriel@gmail.com>
2012-10-26 22:17:15 -0300

    First power commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apuestaprov2012::Application.config.secret_token = '0a799e9f71b92b5ea8252eaa8648181df82e3dafdb57aa0be5a448a4b24618051fad69be3aea7db9f4404d718d5e608dfb12a340150c8a9604a4a639b9989bf0'
